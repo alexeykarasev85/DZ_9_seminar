777e2baeedab079b50079089875243b43d94ce31
Alexey <alexeykarasev85@gmail.com>
2023-07-12 00:28:45 +0300

    ДЗ к 9 семинару

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void RoadToNumber(int number)
+{
+    if (number == 0)
+        {
+            return;
+        }
+    System.Console.Write($"{" "}{number}");
+    RoadToNumber(number - 1);
+}
+
+System.Console.Write("Введите число больше единицы: ");
+int number = Convert.ToInt32(Console.ReadLine());
+RoadToNumber(number);
+
---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+void SumNumbers (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    SumNumbers(numberM, numberN, sum);
+}
+
+Console.WriteLine("Введите начальное число M:");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите начальное число M:");
+int numberN = Convert.ToInt32(Console.ReadLine());
+SumNumbers(numberM, numberN, 0);
